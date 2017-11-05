a68028ad973cb8d5f1fe9208775e3a0fc4e7f0ca
Perry Mitchell <perry@perrymitchell.net>
2017-11-05 16:55:35 +0200

    Make config merging more sane (no strictness regarding depth)

--- source/config.js
+++ source/config.js
@@ -2,17 +2,11 @@ const merge = require("deepmerge");
 
 function createConfig(configObject, template = {}) {
     return merge(
-        sanitiseObject(template),
-        sanitiseObject(configObject)
+        template,
+        configObject
     );
 }
 
-function sanitiseObject(obj) {
-    return Object
-        .keys(obj)
-        .filter(key => typeof obj[key] !== "object")
-        .reduce((output, key) => {
-            output[key] = obj[key];
-            return output;
-        }, {});
-}
+module.exports = {
+    createConfig
+};
